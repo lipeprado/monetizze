0bc4c9769fada0372cdd0523c7f7e524620c3f04
Filipe Prado <infra@klipbox.com.br>
2017-10-29 14:44:27 -0200

    Initial Commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    console.log("Teste")
+  });
+
+})(jQuery, window, document);
